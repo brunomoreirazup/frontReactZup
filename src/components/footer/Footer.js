4bd84c314e3614e3edc0d9b9acd32423d4bb6048
Gabriel Leite <gabriel.leite@zup.com.br>
2018-10-08 15:20:19 -0300

    Foooter

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+
+    render() {
+        return (
+            <nav aria-label="...">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#" tabindex="-1">Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active">
+                        <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
