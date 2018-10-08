2a62b365ad3ba56884ab9a577cd44694067fa0a7
brunomoreirazup <bruno.moreira@zup.com.br>
2018-10-08 15:22:17 -0300

    addNavbar

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from '../navbar/Navbar';
+
+export default class Home extends Component{
+
+
+    render(){
+        return(
+            <Navbar/>
+        )
+    }
+}
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+
+export default class Navbar extends Component{
+
+
+    render(){
+        return(
+            <nav className="navbar navbar-dark bg-dark">
+                <ul className="nav justify-content-center">
+                    <li className="navbar-item">
+                        <Link className="navbar-brand" to="/">Front-End</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="nav-link" to="#">Cidades</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Clientes</Link>
+                    </li>
+                </ul>
+            </nav>
+
+        )
+    }
+}
