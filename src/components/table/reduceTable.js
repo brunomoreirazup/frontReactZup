d53e583185acb07342a2b6c1dd41313ef24deef1
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-10 10:01:31 -0300

    teste

--- src/components/table/reduceTable.js
+++ src/components/table/reduceTable.js
@@ -2,13 +2,13 @@ export default function reduce(state={}, action) {
     console.log("REDUCE TABLE");
     console.log(state);
     console.log(action);
-    switch (action.type) {
-        case "TABLE_HEAD":
-           return state;
-        case "TABLE_BODY":
-            return state;
-        default:
-            console.log(state);
-            return state;
+    if(action.type === "TABLE_BODY")
+    {
+        let newState = Object.assign({},state);
+        newState.table_body = action.table_body;
+        console.log(newState);
+        return newState;
+
     }
+    return state;
 }
