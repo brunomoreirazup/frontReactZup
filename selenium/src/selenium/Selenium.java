ff144582abfbe25d2ab8b92419e4d46a58a7a862
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-15 18:05:40 -0300

    selenium

---
+++ selenium/src/selenium/Selenium.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author vinicius
+ */
+public class Selenium {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       TestCity t = new TestCity();
+       t.run();
+    }
+    
+}
