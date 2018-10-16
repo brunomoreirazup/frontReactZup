99ff4dc02452485534235274808efddd447eed20
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-16 09:19:07 -0300

    auto complete

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import Navbar from '../navbar/Navbar';
+import Autocomplete from "react-autocomplete";
+export default class Home extends Component {
 
-export default class Home extends Component{
 
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <Navbar/>
+                <Navbar />
 
                 <h1>Bem Vindo</h1>
-
+                <Autocomplete
+                    getItemValue={(item) => item.label}
+                    items={[
+                        { label: 'apple' },
+                        { label: 'banana' },
+                        { label: 'pear' }
+                    ]}
+                    renderItem={(item, isHighlighted) =>
+                        <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                            {item.label}
+                        </div>
+                    }
+                    // value={value}
+                    // onChange={(e) => value = e.target.value}
+                    // onSelect={(val) => value = val}
+                />
             </div>
 
         )
