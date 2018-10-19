03368f510bc9487ffd92bb2efff819542f3860a2
brunomoreirazup <bruno.moreira@zup.com.br>
2018-10-19 11:33:34 -0300

    esLint_firstCommit

--- src/components/header/BtnAdd.js
+++ src/components/header/BtnAdd.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
-export default class BtnAdd extends Component{
-
-
-    render(){
-        return(
+export default class BtnAdd extends Component {
+    render( {
+        return (
             <button type='button' id="btAdd" className='btn btn-success' onClick={this.props.click}>Adiciona {this.props.title}</button>
         );
     }
