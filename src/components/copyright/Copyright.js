fbcf0fda470a0c52f5806bcb9a5e78bc699a923b
brunomoreirazup <bruno.moreira@zup.com.br>
2018-10-22 15:05:49 -0300

    TheLastOne

--- src/components/copyright/Copyright.js
+++ src/components/copyright/Copyright.js
@@ -7,7 +7,7 @@ export default class Copyright extends Component {
         return (
             <footer className="page-footer font-small blue">
                 <div className="footer-copyright text-center py-3">2018 Copyright:
-                            <a href="https://github.com/brunomoreirazup/frontReactZup"> Zup RW Internship Team</a>
+                            <a href="https://github.com/brunomoreirazup/frontReactZup" target="_blank"> Zup RW Internship Team</a>
                 </div>
             </footer>            
         );
