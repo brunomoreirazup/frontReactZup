01197dfdfd7e1329bcc95a5928dcc6e89e377d22
Murilo Andrade <murilo.andrade@zup.com.br>
2018-10-23 14:07:04 -0300

    EsLintButtons

---
+++ src/components/button/btEdit/BtEdit.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../buttonCss/button.css';
+import PropTypes from 'prop-types';
+
+export default function BtEdit(props) {
+  const { onClick } = props;
+  return (
+    <React.Fragment>
+      <button
+        type="button"
+        className="btn btn-info bt-table bt-edit"
+        onClick={onClick}
+      />
+    </React.Fragment>
+  );
+}
+
+
+BtEdit.propTypes = {
+  onClick: PropTypes.isRequired,
+};
