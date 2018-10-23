05d3d4d7e4b39deac68a11778c9d6d89bc97dec6
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-23 12:06:44 -0300

    reduceFooter sem comentario

--- src/components/footer/reduceFooter.js
+++ src/components/footer/reduceFooter.js
@@ -9,43 +9,5 @@ export default function reduce(state = {}, action) {
     const newState = Object.assign({}, state);
     newState.pages.currentPage = action.currentPage;
   }
-
-  // if (action.type === "PAGE_SIZE") {
-
-  //     let newState = Object.assign({}, state);
-  //     newState.page_size = action.page_size;
-  //     console.log(newState);
-  //     return newState;
-
-  // }
-  // if (action.type === "TABLE_BODY") {
-  //     let newState = Object.assign({}, state);
-  //     newState.table_body = action.table_body;
-  //     console.log(newState);
-  //     return newState;
-  // }
-
-
-  // if(action.type === "TOGGLE_MAIN_MODAL")
-  // {
-  //     let newState = Object.assign({},state);
-  //     if(action.modalOpen === undefined)
-  //         newState.modalOpen = !newState.modalOpen;
-  //     else
-  //         newState.modalOpen = action.modalOpen;
-  //     console.log(newState);
-  //     return newState;
-
-
-  // }
-  // if (action.type === "MAIN_MODAL_CONTENT") {
-  //     let newState = Object.assign({}, state);
-  //     newState.modalContent = action.modalContent;
-  //     console.log(newState);
-  //     return newState;
-
-
-  // }
-
   return state;
 }
