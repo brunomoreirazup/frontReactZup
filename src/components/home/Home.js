1e9d9a449ab7b457a8d4a53ec00aed347fb0ba4b
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-23 10:42:03 -0300

    dashboard  is missing in props validation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
                 <div className="full-size">
                     <Navbar />
                     <h1 className='welcome'>Bem-vindo!</h1>
-                    <img src={image} className='home-image' alt='home'></img>
+                    {/* <img src={image} className='home-image' alt='home'></img> */}
                 </div>                
             </div>
 
