1d8c937deba147171b456af95820ac62935aa019
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-23 13:09:42 -0300

    table ok

--- src/components/table/reduceTable.js
+++ src/components/table/reduceTable.js
@@ -1,19 +1,19 @@
 export default function reduce(state = {}, action) {
-    if (action.type === "TABLE_BODY") {
-        let newState = Object.assign({}, state);
-        newState.table_body = action.table_body;
-        return newState;
-    }
+  if (action.type === 'TABLE_BODY') {
+    const newState = Object.assign({}, state);
+    newState.table_body = action.table_body;
+    return newState;
+  }
 
-    if (action.type === "SORT") {
-        let newState = Object.assign({}, state);
-        if(action.sort_order) {
-            newState.sort_order= action.sort_order;
-        } else  if (newState.sort_order === 'asc') {
-            newState.sort_order = 'desc';
-        } else newState.sort_order = 'asc';
-        return newState;
-    }
-    return state;
+  if (action.type === 'SORT') {
+    const newState = Object.assign({}, state);
+    if (action.sort_order) {
+      newState.sort_order = action.sort_order;
+    } else if (newState.sort_order === 'asc') {
+      newState.sort_order = 'desc';
+    } else newState.sort_order = 'asc';
+    return newState;
+  }
+  return state;
 
 }
