3c045a84db71bc9ec08d614f79413ff113d932c4
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-23 12:00:37 -0300

    Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.clearPages();
+  }
+
+  clearPages() {
+    const { dispatch } = this.props;
+    const defaultPages = {
+      homePage: 1,
+      lastPage: 1,
+      currentPage: 1,
+
+    };
+    dispatch({ type: 'PAGES', pages: defaultPages });
+  }
+
+  printPagination() {
+    const { reduceFooter, changeCurrentPage } = this.props;
+    if (reduceFooter != null && reduceFooter.pages != null) {
+      const {
+        currentPage,
+        homePage,
+        lastPage,
+      } = reduceFooter.pages;
+      return (
+        <div className="container">
+          <nav>
+            <ul className="pagination">
+              <li className={currentPage !== homePage ? 'page-item' : 'page-item disabled'}>
+                <button
+                  onClick={
+                    currentPage !== homePage ? () => changeCurrentPage(currentPage - 1) : () => {}
+                  }
+                  type="button"
+                  className="page-link _page-link"
+                  id="prevPageBt"
+                  tabIndex="-1"
+                >
+                    Anterior
+                </button>
+
+              </li>
+
+              {currentPage - 1 > homePage ? (
+
+                <li className="page-item">
+                  <button
+                    onClick={() => changeCurrentPage(homePage)}
+                    type="button"
+                    className="page-link _page-link"
+                    id="homePage"
+                  >
+                    {homePage}
+                  </button>
+                </li>
+
+              ) : ''}
+
+              {currentPage - 2 > homePage ? '...' : ''}
+
+              {currentPage !== homePage ? (
+                <li className="page-item">
+                  <button
+                    onClick={() => changeCurrentPage(currentPage - 1)}
+                    type="button"
+                    className="page-link _page-link"
+                    id="prevPage"
+                  >
+                    {currentPage - 1}
+                  </button>
+                </li>
+
+              ) : ''}
+
+              <li className="page-item active ">
+                <button type="button" className="_pagination-active page-link" href="#">
+                  {currentPage}
+                  <span className="sr-only">(current)</span>
+                </button>
+              </li>
+
+              {currentPage !== lastPage ? (
+                <li className="page-item">
+                  <button
+                    onClick={() => changeCurrentPage(currentPage + 1)}
+                    type="button"
+                    className="page-link _page-link"
+                    id="nextPage"
+                  >
+                    {currentPage + 1}
+                  </button>
+                </li>
+
+              ) : ''}
+
+              {currentPage + 2 < lastPage ? '...' : ''}
+
+              {currentPage + 1 < lastPage ? (
+
+                <li className="page-item">
+                  <button
+                    onClick={() => changeCurrentPage(lastPage)}
+                    type="button"
+                    className="page-link _page-link"
+                    id="lastPage"
+                  >
+                    {lastPage}
+                  </button>
+                </li>
+
+              ) : ''}
+
+              <li className={currentPage !== lastPage ? 'page-item' : 'page-item disabled'}>
+                <button
+                  onClick={
+                    currentPage !== lastPage ? () => changeCurrentPage(currentPage + 1) : () => {}
+                  }
+                  type="button"
+                  className="page-link _page-link"
+                  id="nextPageBt"
+                >
+                  Próximo
+                </button>
+
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+
+      );
+    }
+    return '';
+  }
+
+  render() {
+    return (
+      <div>
+        {this.printPagination()}
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {
+    reduceFooter: state.reduceFooter,
+  };
+}
+Footer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  reduceFooter: PropTypes.func.isRequired,
+  changeCurrentPage: PropTypes.func.isRequired,
+};
+export default connect(mapStateToProps)(Footer);
