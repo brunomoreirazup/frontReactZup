1fb12e23f18d4f179633d7eec4adeddaf14800c8
brunomoreirazup <bruno.moreira@zup.com.br>
2019-04-17 10:55:19 -0300

    build po lucas

---
+++ build/precache-manifest.79f6dabcc57842d1201f832b227ba0a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4a10743cbe78620fc59f63d3daf8ca0",
+    "url": "/static/media/trash.f4a10743.png"
+  },
+  {
+    "revision": "bcb2fdbfcdbcafb76618826501ae8c50",
+    "url": "/static/media/loading.bcb2fdbf.gif"
+  },
+  {
+    "revision": "3f32669882a6a0d49d1bf93b6acedfee",
+    "url": "/static/media/edit.3f326698.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "054106f2bde77f3c5e63",
+    "url": "/static/js/main.054106f2.chunk.js"
+  },
+  {
+    "revision": "1bf904e1e35ac6c1f53c",
+    "url": "/static/js/1.1bf904e1.chunk.js"
+  },
+  {
+    "revision": "054106f2bde77f3c5e63",
+    "url": "/static/css/main.a6fc6ceb.chunk.css"
+  },
+  {
+    "revision": "1bf904e1e35ac6c1f53c",
+    "url": "/static/css/1.2faf888b.chunk.css"
+  },
+  {
+    "revision": "cdbccff4be4944aaa12f2797aad6d907",
+    "url": "/index.html"
+  }
+];
